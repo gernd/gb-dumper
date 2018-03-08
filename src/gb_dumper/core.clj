ccd8b509bb9a454df2759abde97c758e5205bc37
Gernot Pointner <gernot.pointner@jambit.com>
2018-03-08 17:02:31 +0100

    Add usage statement

--- src/gb_dumper/core.clj
+++ src/gb_dumper/core.clj
@@ -1,7 +1,13 @@
 (ns gb-dumper.core
   (:gen-class))
 
+(defn print-usage
+  []
+  (println "Usage: executable <PATH-TO-GB-ROM>"))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (if (not (= 1 (count args)))
+    (print-usage)
+    (println "Analyzing GB Rom File")))
