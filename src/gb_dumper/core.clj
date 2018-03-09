f12c93829a3d4f9b59640774b777776ddb4b99f7
Gernot Pointner <gernot.pointner@googlemail.com>
2018-03-09 17:37:09 +0100

    Read ROM from file

--- src/gb_dumper/core.clj
+++ src/gb_dumper/core.clj
@@ -1,17 +1,21 @@
 (ns gb-dumper.core
   (:gen-class))
 
+(require '(clojure.java [io :as io]))
+
 (defn print-usage
   "Prints usage information on howto use the GB ROM Analyzer"
   []
   (println "Usage: executable <PATH-TO-GB-ROM>"))
 
+
 (defn read-rom
-  [path-to-rom]
-  with-open [in (input-stream (file path-to-rom))]
-  (let [buf (byte-array 1000)
-        n (.read in buf)]
-    (println "Read" n "bytes.")))
+  "Reads the given ROM into a byte array"
+  [file]
+  (with-open [xin (io/input-stream file)
+              xout (java.io.ByteArrayOutputStream.)]
+    (io/copy xin xout)
+    (.toByteArray xout)))
 
 (defn -main
   "Analyzes the GB ROM file given as the first parameter "
@@ -20,5 +24,5 @@
     (print-usage)
     (let [file-name (first args)]
     (do (println "Analyzing GB Rom File " file-name)
-        
-        )))
+        (let [rom-binary-data (read-rom file-name)]
+        (println "rom binary data " rom-binary-data))))))
