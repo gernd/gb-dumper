4c22f805e6aa7b9a6b39e34612a4d3601f248b22
Gernot Pointner <gernot.pointner@googlemail.com>
2018-03-22 23:23:28 +0100

    Add test and impl for rst08 adress, prepend 0x when hexifying byte arrays

--- src/gb_dumper/analyze.clj
+++ src/gb_dumper/analyze.clj
@@ -4,7 +4,7 @@
 (defn to-hex-string
   "Convert a seq of bytes into a hex encoded string"
   [bytes]
-  (apply str (for [byte bytes] (format "%02x" byte))))
+  (apply str "0x" (for [byte bytes] (format "%02x" byte))))
 
 (defn unpack-rom-data
   "unpacks the gb rom data given as byte array"
@@ -12,5 +12,6 @@
   (let [byte-buffer (ByteBuffer/allocate (count rom-data))]
     (.put byte-buffer rom-data 0 (count rom-data))          ; fill byte buffer with rom data
     (.flip byte-buffer)                                     ; reset pointer for reading
-    (let [rst$00 (.get byte-buffer 0)]
-      {:rst$00 rst$00})))
+    (let [rst$00 (.get byte-buffer)
+          rst$08 (.get byte-buffer)]
+      {:rst$00 rst$00 :rst$08 rst$08})))
--- test/gb_dumper/core_test.clj
+++ test/gb_dumper/core_test.clj
@@ -7,9 +7,10 @@
 (defn prepare-test-data
   "Prepares test ROM data and returns it as byte[]"
   []
-  (let [byte-buffer (ByteBuffer/allocate 1)
-        buf (byte-array 1)]
+  (let [byte-buffer (ByteBuffer/allocate 2)
+        buf (byte-array 2)]
     (.put byte-buffer (.byteValue 0x12))                    ; rst$00
+    (.put byte-buffer (.byteValue 0x33))                    ; rst$08
     (.flip byte-buffer)
     (.get byte-buffer buf)
     buf))
@@ -19,3 +20,9 @@
     (let [test-rom-data (prepare-test-data)]
       (do (println "Test ROM data is " (to-hex-string test-rom-data))
           (is (= 0x12 (get (unpack-rom-data test-rom-data) :rst$00)))))))
+
+(deftest test-read-$08-address
+  (testing "Checks that the restart $08 adress is correctly read from the ROM"
+    (let [test-rom-data (prepare-test-data)]
+      (do (println "Test ROM data is " (to-hex-string test-rom-data))
+          (is (= 0x33 (get (unpack-rom-data test-rom-data) :rst$08)))))))
