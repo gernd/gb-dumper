cd0e0642bffaa5baa1279c5f2ca216aa91c95768
Gernot Pointner <gernot.pointner@googlemail.com>
2018-03-23 20:14:04 +0100

    Add project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject gb-dumper "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A simple GB ROM dumper for the command line"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
