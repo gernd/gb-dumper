86c2d8ca5393d5052db8a8afb8008608b0500886
Gernot Pointner <gernot.pointner@googlemail.com>
2018-03-23 20:09:00 +0100

    Add implementation and test for unpacking restart addresses

--- src/gb_dumper/analyze.clj
+++ src/gb_dumper/analyze.clj
@@ -13,5 +13,12 @@
     (.put byte-buffer rom-data 0 (count rom-data))          ; fill byte buffer with rom data
     (.flip byte-buffer)                                     ; reset pointer for reading
     (let [rst$00 (.get byte-buffer)
-          rst$08 (.get byte-buffer)]
-      {:rst$00 rst$00 :rst$08 rst$08})))
+          rst$08 (.get byte-buffer)
+          rst$10 (.get byte-buffer)
+          rst$18 (.get byte-buffer)
+          rst$20 (.get byte-buffer)
+          rst$28 (.get byte-buffer)
+          rst$30 (.get byte-buffer)
+          rst$38 (.get byte-buffer)]
+      {:rst$00 rst$00 :rst$08 rst$08 :rst$10 rst$10 :rst$18 rst$18
+       :rst$20 rst$20 :rst$28 rst$28 :rst$30 rst$30 :rst$38 rst$38})))
--- test/gb_dumper/core_test.clj
+++ test/gb_dumper/core_test.clj
@@ -7,17 +7,31 @@
 (defn prepare-test-data
   "Prepares test ROM data and returns it as byte[]"
   []
-  (let [byte-buffer (ByteBuffer/allocate 2)
-        buf (byte-array 2)]
+  (let [test-rom-size 8
+        byte-buffer (ByteBuffer/allocate test-rom-size)
+        buf (byte-array test-rom-size)]
     (.put byte-buffer (.byteValue 0x12))                    ; rst$00
     (.put byte-buffer (.byteValue 0x33))                    ; rst$08
+    (.put byte-buffer (.byteValue 0x00))                    ; rst$10
+    (.put byte-buffer (.byteValue 0x12))                    ; rst$18
+    (.put byte-buffer (.byteValue 0x13))                    ; rst$20
+    (.put byte-buffer (.byteValue 0x03))                    ; rst$28
+    (.put byte-buffer (.byteValue 0x24))                    ; rst$30
+    (.put byte-buffer (.byteValue 0x42))                    ; rst$38
     (.flip byte-buffer)
     (.get byte-buffer buf)
     buf))
 
-(deftest test-read-$00-address
+(deftest test-unpack-rom-data-restart-addresses-correct
   (testing "Checks that the restart adresses are correctly read from the ROM"
-    (let [test-rom-data (prepare-test-data)]
+    (let [test-rom-data (prepare-test-data)
+          unpacked-rom-data (unpack-rom-data test-rom-data)]
       (do (println "Test ROM data is" (to-hex-string test-rom-data))
-          (is (= 0x12 (get (unpack-rom-data test-rom-data) :rst$00))
-          (is (= 0x33 (get (unpack-rom-data test-rom-data) :rst$08))))))))
+          (is (= 0x12 (get unpacked-rom-data :rst$00)))
+          (is (= 0x33 (get unpacked-rom-data :rst$08)))
+          (is (= 0x00 (get unpacked-rom-data :rst$10)))
+          (is (= 0x12 (get unpacked-rom-data :rst$18)))
+          (is (= 0x13 (get unpacked-rom-data :rst$20)))
+          (is (= 0x03 (get unpacked-rom-data :rst$28)))
+          (is (= 0x24 (get unpacked-rom-data :rst$30)))
+          (is (= 0x42 (get unpacked-rom-data :rst$38)))))))
