634964ffa655e3fc9692acc815c64f9ea5e46cb4
Gernot Pointner <gernot.pointner@googlemail.com>
2018-03-28 21:59:25 +0200

    Add unpacking and test of interrupt addresses

--- src/gb_dumper/analyze.clj
+++ src/gb_dumper/analyze.clj
@@ -12,13 +12,35 @@
   (let [byte-buffer (ByteBuffer/allocate (count rom-data))]
     (.put byte-buffer rom-data 0 (count rom-data))          ; fill byte buffer with rom data
     (.flip byte-buffer)                                     ; reset pointer for reading
-    (let [rst$00 (.get byte-buffer)
+    (let [
+          ; restart addresses
+          rst$00 (.get byte-buffer)
           rst$08 (.get byte-buffer)
           rst$10 (.get byte-buffer)
           rst$18 (.get byte-buffer)
           rst$20 (.get byte-buffer)
           rst$28 (.get byte-buffer)
           rst$30 (.get byte-buffer)
-          rst$38 (.get byte-buffer)]
-      {:rst$00 rst$00 :rst$08 rst$08 :rst$10 rst$10 :rst$18 rst$18
-       :rst$20 rst$20 :rst$28 rst$28 :rst$30 rst$30 :rst$38 rst$38})))
+          rst$38 (.get byte-buffer)
+          ; interrupt addresses
+          vertical-blank-interrupt (.get byte-buffer)
+          lcdc-status-interrupt (.get byte-buffer)
+          timer-overflow-interrupt (.get byte-buffer)
+          serial-transfer-completion-interrupt (.get byte-buffer)
+          high-to-low-interrupt (.get byte-buffer)
+          ]
+      {
+       :rst$00                               rst$00
+       :rst$08                               rst$08
+       :rst$10                               rst$10
+       :rst$18                               rst$18
+       :rst$20                               rst$20
+       :rst$28                               rst$28
+       :rst$30                               rst$30
+       :rst$38                               rst$38
+       :vertical-blank-interrupt             vertical-blank-interrupt
+       :lcdc-status-interrupt                lcdc-status-interrupt
+       :timer-overflow-interrupt             timer-overflow-interrupt
+       :serial-transfer-completion-interrupt serial-transfer-completion-interrupt
+       :high-to-low-interrupt                high-to-low-interrupt
+       })))
--- test/gb_dumper/analyze_test.clj
+++ test/gb_dumper/analyze_test.clj
@@ -7,7 +7,7 @@
 (defn prepare-test-data
   "Prepares test ROM data and returns it as byte[]"
   []
-  (let [test-rom-size 8
+  (let [test-rom-size 13
         byte-buffer (ByteBuffer/allocate test-rom-size)
         buf (byte-array test-rom-size)]
     (.put byte-buffer (.byteValue 0x12))                    ; rst$00
@@ -18,6 +18,11 @@
     (.put byte-buffer (.byteValue 0x03))                    ; rst$28
     (.put byte-buffer (.byteValue 0x24))                    ; rst$30
     (.put byte-buffer (.byteValue 0x99))                    ; rst$38
+    (.put byte-buffer (.byteValue 0x01))                    ; vertical blank interrupt
+    (.put byte-buffer (.byteValue 0x02))                    ; lcdc state interrupt
+    (.put byte-buffer (.byteValue 0x03))                    ; timer overflow interrupt
+    (.put byte-buffer (.byteValue 0x04))                    ; serial transfer complete interrupt
+    (.put byte-buffer (.byteValue 0x05))                    ; high to low interrupt
     (.flip byte-buffer)
     (.get byte-buffer buf)
     buf))
@@ -27,6 +32,7 @@
   [first-byte second-byte]
   (is (= (.byteValue first-byte) (.byteValue second-byte))))
 
+; TODO: split into two tests
 (deftest test-unpack-rom-data-restart-addresses-correct
   (testing "Checks that the restart adresses are correctly read from the ROM"
     (let [test-rom-data (prepare-test-data)
@@ -39,4 +45,9 @@
           (is-same-byte 0x13 (get unpacked-rom-data :rst$20))
           (is-same-byte 0x03 (get unpacked-rom-data :rst$28))
           (is-same-byte 0x24 (get unpacked-rom-data :rst$30))
-          (is-same-byte 0x99 (get unpacked-rom-data :rst$38))))))
+          (is-same-byte 0x99 (get unpacked-rom-data :rst$38))
+          (is-same-byte 0x01 (get unpacked-rom-data :vertical-blank-interrupt))
+          (is-same-byte 0x02 (get unpacked-rom-data :lcdc-status-interrupt))
+          (is-same-byte 0x03 (get unpacked-rom-data :timer-overflow-interrupt))
+          (is-same-byte 0x04 (get unpacked-rom-data :serial-transfer-completion-interrupt))
+          (is-same-byte 0x05 (get unpacked-rom-data :high-to-low-interrupt))))))
