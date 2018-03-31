c10392e93e306a30faf201e479d08034aaeb8e85
Gernot Pointner <gernot.pointner@googlemail.com>
2018-03-31 22:07:40 +0200

    Add separate test for interrupt addresses

--- test/gb_dumper/analyze_test.clj
+++ test/gb_dumper/analyze_test.clj
@@ -32,12 +32,11 @@
   [first-byte second-byte]
   (is (= (.byteValue first-byte) (.byteValue second-byte))))
 
-; TODO: split into two tests
 (deftest test-unpack-rom-data-restart-addresses-correct
   (testing "Checks that the restart adresses are correctly read from the ROM"
     (let [test-rom-data (prepare-test-data)
           unpacked-rom-data (unpack-rom-data test-rom-data)]
-      (do (println "Test ROM data is" (to-hex-string test-rom-data))
+      (do (println "Test ROM data is" (to-hex-string test-rom-data) " unpacked ROM data is " unpacked-rom-data)
           (is-same-byte 0x12 (get unpacked-rom-data :rst$00))
           (is-same-byte 0x33 (get unpacked-rom-data :rst$08))
           (is-same-byte 0x00 (get unpacked-rom-data :rst$10))
@@ -45,7 +44,13 @@
           (is-same-byte 0x13 (get unpacked-rom-data :rst$20))
           (is-same-byte 0x03 (get unpacked-rom-data :rst$28))
           (is-same-byte 0x24 (get unpacked-rom-data :rst$30))
-          (is-same-byte 0x99 (get unpacked-rom-data :rst$38))
+          (is-same-byte 0x99 (get unpacked-rom-data :rst$38))))))
+
+(deftest test-unpack-rom-data-interrupt-addresses-correct
+  (testing "Checks that the interrupt adresses are correctly read from the ROM"
+    (let [test-rom-data (prepare-test-data)
+          unpacked-rom-data (unpack-rom-data test-rom-data)]
+      (do (println "Test ROM data is" (to-hex-string test-rom-data) " unpacked ROM data is " unpacked-rom-data)
           (is-same-byte 0x01 (get unpacked-rom-data :vertical-blank-interrupt))
           (is-same-byte 0x02 (get unpacked-rom-data :lcdc-status-interrupt))
           (is-same-byte 0x03 (get unpacked-rom-data :timer-overflow-interrupt))
