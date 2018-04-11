0557a1b0b5d2b87ae0fdb0f08fc8d827e585164a
Gernot Pointner <gernot.pointner@googlemail.com>
2018-04-11 06:53:26 +0200

    Change license to MIT license

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
 ## [Unreleased]
+### Changed
+License change to MIT license
+
 ### Removed
 Unused docs
 
--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject gb-dumper "0.1.0-SNAPSHOT"
   :description "A simple GB ROM dumper for the command line"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "LICENSE.md"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot gb-dumper.core
   :target-path "target/%s"
